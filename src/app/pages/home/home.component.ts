7c730b1572e7caff561ed15915e5c75a68f7f992
Ivan Villacorta <ivan_mv23@hotmail.com>
2024-03-18 08:14:30 -0300

    feat: Add component search and list

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { PokeSearchComponent } from '../../shared/components/poke-search/poke-search.component';
+import { PokeListComponent } from '../../shared/components/poke-list/poke-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    PokeSearchComponent,
+    PokeListComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
